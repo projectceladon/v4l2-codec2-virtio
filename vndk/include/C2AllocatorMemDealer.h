1c4a03feeb8f7f2a10c8bd01bccded773a34f8e1
Johny Lin <johnylin@google.com>
2017-10-18 20:36:02 +0800

    codec2: C2Buffer MemoryDealer and SurfaceComposer implementation
    Use interface in C2VDASupport.h to get allocator store, or to get block pool by
    pool ID.
    
    In C2VDAAllocatorStore, use C2AllocatorMemDealer as default linear allocator,
    and C2AllocatorCrosGralloc as default graphic allocator.
    
    Since for now we test it mostly on NYC branch, CrosGralloc had only finished
    NYC version and I split the file into two and distinguish them by
    PLATFORM_VERSION in makefile.
    
    Bug: 63828240
    Test: mmm external/v4l2_codec2
    Change-Id: I5e482fef1c3d9361882c39554bb97688ccc799b8

---
+++ vndk/include/C2AllocatorMemDealer.h
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef C2_ALLOCATOR_MEM_DEALER_H_
+#define C2_ALLOCATOR_MEM_DEALER_H_
+
+#include <C2Buffer.h>
+
+namespace android {
+
+class C2AllocatorMemDealer : public C2Allocator {
+public:
+    virtual id_t getId() const override;
+
+    virtual C2String getName() const override;
+
+    virtual std::shared_ptr<const Traits> getTraits() const override {
+        return nullptr; // TODO
+    }
+
+    virtual c2_status_t newLinearAllocation(
+            uint32_t capacity, C2MemoryUsage usage,
+            std::shared_ptr<C2LinearAllocation> *allocation) override;
+
+    C2AllocatorMemDealer();
+    virtual ~C2AllocatorMemDealer();
+};
+
+} // namespace android
+
+#endif // C2_ALLOCATOR_MEM_DEALER_H_
