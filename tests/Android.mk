44f88cecf2cdfdc16cd3076fec75846700d6bc1d
Pin-chih Lin <johnylin@google.com>
2019-01-11 18:30:49 +0800

    C2VEACompIntf_test: C2VEAComponent interface native test
    Implement C2VEACompIntf_test for testing all parameters in
    C2VEAComponent.
    
    Bug: 73059339
    Test: mmm external/v4l2_codec2
    Test: adb push out/target/product/cheets_x86/data/nativetest/C2VEACompIntf_test /data/local/tmp/
    Test: adb shell /data/local/tmp/C2VEACompIntf_test/C2VEACompIntf_test
    Change-Id: I4f6c77f525c96c8ff2e49bb211af033a656f5eb4
    (cherry picked from commit 63cf96636c2592201d38097b08df20e278130c25)

--- tests/Android.mk
+++ tests/Android.mk
@@ -40,6 +40,42 @@ include $(BUILD_NATIVE_TEST)
 include $(CLEAR_VARS)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 
+LOCAL_MODULE := C2VEACompIntf_test
+
+LOCAL_MODULE_TAGS := tests
+
+LOCAL_SRC_FILES := \
+  C2CompIntfTest.cpp \
+  C2VEACompIntf_test.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+  libchrome \
+  libcutils \
+  liblog \
+  libstagefright_codec2 \
+  libstagefright_codec2_vndk \
+  libutils \
+  libv4l2_codec2 \
+  libv4l2_codec2_vda \
+
+LOCAL_C_INCLUDES += \
+  $(TOP)/external/v4l2_codec2/include \
+  $(TOP)/external/v4l2_codec2/vda \
+  $(TOP)/hardware/google/av/codec2/include \
+  $(TOP)/hardware/google/av/codec2/vndk/include \
+  $(TOP)/hardware/google/av/media/codecs/base/include \
+
+LOCAL_CFLAGS += -Werror -Wall -std=c++14
+LOCAL_CLANG := true
+
+LOCAL_LDFLAGS := -Wl,-Bsymbolic
+
+include $(BUILD_NATIVE_TEST)
+
+
+include $(CLEAR_VARS)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+
 LOCAL_MODULE := C2VDAComponent_test
 
 LOCAL_MODULE_TAGS := tests
