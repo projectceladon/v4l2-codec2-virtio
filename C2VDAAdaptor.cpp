d90af98084d0851c476a023798b3cb175c96b9a7
Pin-chih Lin <johnylin@google.com>
2019-05-15 20:49:09 +0800

    C2VDAAdaptor: fix SharedMemoryHandle usage from libchrome uprev
    libchrome uprev: ag/5222617
    
    Bug: 129747937
    Test: build bertha_x86_64-eng on pi-arcvm-dev branch
    Change-Id: I616f597173b024777198a79bccaff190d16dda05
    (cherry picked from commit 55231026bdba84476fd3c8b8d33c581419f9c494)

--- C2VDAAdaptor.cpp
+++ C2VDAAdaptor.cpp
@@ -56,8 +56,9 @@ VideoDecodeAcceleratorAdaptor::Result C2VDAAdaptor::initialize(
 
 void C2VDAAdaptor::decode(int32_t bitstreamId, int ashmemFd, off_t offset, uint32_t bytesUsed) {
     CHECK(mVDA);
-    mVDA->Decode(media::BitstreamBuffer(bitstreamId, base::SharedMemoryHandle(ashmemFd, true),
-                                        bytesUsed, offset));
+    ::base::SharedMemoryHandle shmHandle(::base::FileDescriptor(ashmemFd, true), 0u,
+                                         ::base::UnguessableToken::Create());
+    mVDA->Decode(media::BitstreamBuffer(bitstreamId, shmHandle, bytesUsed, offset));
 }
 
 void C2VDAAdaptor::assignPictureBuffers(uint32_t numOutputBuffers) {
@@ -90,7 +91,7 @@ void C2VDAAdaptor::importBufferForPicture(int32_t pictureBufferId, HalPixelForma
     }
 
     media::NativePixmapHandle handle;
-    handle.fds.emplace_back(base::FileDescriptor(dmabufFd, true));
+    handle.fds.emplace_back(::base::FileDescriptor(dmabufFd, true));
     for (const auto& plane : planes) {
         handle.planes.emplace_back(plane.mStride, plane.mOffset, 0, 0);
     }
