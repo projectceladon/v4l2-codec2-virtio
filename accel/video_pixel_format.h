49c28c5b3f20d449b001f15f9e6f10da4334804c
Pin-chih Lin <johnylin@google.com>
2019-08-08 12:10:59 +0800

    accel: port the latest video_pixel_format.(h|cc)
    Note: the original code is under video_types.(h|cc) in Chromium
    
    Bug: 73059339
    Test: compile image
    Change-Id: I3a5a71d2a9ea076e6e6807a4bb5342f4c8279624
    (cherry picked from commit 24ce720487f1c92229d50e0018631f7eef7a928a)

--- accel/video_pixel_format.h
+++ accel/video_pixel_format.h
@@ -1,11 +1,13 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// Note: ported from Chromium commit head: 006301b
+// Note: ported from Chromium commit head: 98751c5
 // Note: only necessary functions are ported from video_types.h
 
 #ifndef VIDEO_PIXEL_FORMAT_H_
 #define VIDEO_PIXEL_FORMAT_H_
+
+#include <string>
 
 namespace media {
 
@@ -13,6 +15,9 @@ namespace media {
 // http://www.fourcc.org/rgb.php and http://www.fourcc.org/yuv.php
 // Logged to UMA, so never reuse values. Leave gaps if necessary.
 // Ordered as planar, semi-planar, YUV-packed, and RGB formats.
+// When a VideoFrame is backed by native textures, VideoPixelFormat describes
+// how those textures should be sampled and combined to produce the final
+// pixels.
 enum VideoPixelFormat {
   PIXEL_FORMAT_UNKNOWN = 0,  // Unknown or unspecified format value.
   PIXEL_FORMAT_I420 =
@@ -33,10 +38,11 @@ enum VideoPixelFormat {
       8,  // 16bpp interleaved 2x1 U, 1x1 Y, 2x1 V, 1x1 Y samples.
   PIXEL_FORMAT_YUY2 =
       9,  // 16bpp interleaved 1x1 Y, 2x1 U, 1x1 Y, 2x1 V samples.
-  PIXEL_FORMAT_ARGB = 10,   // 32bpp ARGB, 1 plane.
-  PIXEL_FORMAT_XRGB = 11,   // 24bpp XRGB, 1 plane.
-  PIXEL_FORMAT_RGB24 = 12,  // 24bpp BGR, 1 plane.
-  PIXEL_FORMAT_RGB32 = 13,  // 32bpp BGRA, 1 plane.
+  PIXEL_FORMAT_ARGB = 10,   // 32bpp BGRA (byte-order), 1 plane.
+  PIXEL_FORMAT_XRGB = 11,   // 24bpp BGRX (byte-order), 1 plane.
+  PIXEL_FORMAT_RGB24 = 12,  // 24bpp BGR (byte-order), 1 plane.
+
+  /* PIXEL_FORMAT_RGB32 = 13,  Deprecated */
   PIXEL_FORMAT_MJPEG = 14,  // MJPEG compressed.
   // MediaTek proprietary format. MT21 is similar to NV21 except the memory
   // layout and pixel layout (swizzles). 12bpp with Y plane followed by a 2x2
@@ -48,15 +54,15 @@ enum VideoPixelFormat {
   // Plane size = Row pitch * (((height+31)/32)*32)
   PIXEL_FORMAT_MT21 = 15,
 
-  // The P* in the formats below designates the number of bits per pixel. I.e.
-  // P9 is 9-bits per pixel, P10 is 10-bits per pixel, etc.
+  // The P* in the formats below designates the number of bits per pixel
+  // component. I.e. P9 is 9-bits per pixel component, P10 is 10-bits per pixel
+  // component, etc.
   PIXEL_FORMAT_YUV420P9 = 16,
   PIXEL_FORMAT_YUV420P10 = 17,
   PIXEL_FORMAT_YUV422P9 = 18,
   PIXEL_FORMAT_YUV422P10 = 19,
   PIXEL_FORMAT_YUV444P9 = 20,
   PIXEL_FORMAT_YUV444P10 = 21,
-
   PIXEL_FORMAT_YUV420P12 = 22,
   PIXEL_FORMAT_YUV422P12 = 23,
   PIXEL_FORMAT_YUV444P12 = 24,
@@ -64,10 +70,27 @@ enum VideoPixelFormat {
   /* PIXEL_FORMAT_Y8 = 25, Deprecated */
   PIXEL_FORMAT_Y16 = 26,  // single 16bpp plane.
 
+  PIXEL_FORMAT_ABGR = 27,  // 32bpp RGBA (byte-order), 1 plane.
+  PIXEL_FORMAT_XBGR = 28,  // 24bpp RGBX (byte-order), 1 plane.
+
+  PIXEL_FORMAT_P016LE = 29,  // 24bpp NV12, 16 bits per channel
+
   // Please update UMA histogram enumeration when adding new formats here.
   PIXEL_FORMAT_MAX =
-      PIXEL_FORMAT_Y16,  // Must always be equal to largest entry logged.
+      PIXEL_FORMAT_P016LE,  // Must always be equal to largest entry logged.
 };
+
+// Returns the name of a Format as a string.
+std::string VideoPixelFormatToString(VideoPixelFormat format);
+
+// Returns human readable fourcc string.
+// If any of the four characters is non-printable, it outputs
+// "0x<32-bit integer in hex>", e.g. FourccToString(0x66616b00) returns
+// "0x66616b00".
+std::string FourccToString(uint32_t fourcc);
+
+// Returns the number of significant bits per channel.
+size_t BitDepth(VideoPixelFormat format);
 
 }  // namespace media
 
