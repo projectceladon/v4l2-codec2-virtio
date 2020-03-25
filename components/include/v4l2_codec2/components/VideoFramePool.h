6a7255adf147678c349647f0cfb315e6d4eadd68
Chih-Yu Huang <akahuang@google.com>
2020-03-25 17:01:47 +0900

    components: Implement Codec2.0 V4L2 decode component
    This CL implements V4L2DecodeComponent, a decode C2Component via V4L2
    API. It mainly contains these parts:
    
    - VideoDecoder:
      An interface for video decoding, similar to media::VideoDecoder at
    Chromium project.
    - V4L2Decoder:
      An implementation of VideoDecoder via V4L2 stateful API. We wrap all
      V4L2-related logic in this class
    
    - VideoFrame
      The data structure to represent the output buffer of VideoDecoder.
      It encapsulates a C2GraphicBlock and only exposes the essential
      information of physical buffers.
    - VideoFramePool:
      The buffer pool used by VideoDecoder. It encapsulates C2BlockPool
      and provides asynchronous method to fetch buffers.
    
    - V4L2DecodeComponent, V4L2DecodeInterface:
      The C2Component implementation that delegates the decode request to
      V4L2Decoder.
    - V4L2ComponentFactory:
      The C2ComponentFactory that creates V4L2DecodeComponent.
    
    Bug: 152714603
    Test: mmm external/v4l2_codec2/
    Test: Run e2e test and make sure the V4L2DecodeComponent is running
    
    Change-Id: I23e9b3553b62dd6c83d943d7787297305372c37d

---
+++ components/include/v4l2_codec2/components/VideoFramePool.h
@@ -0,0 +1,65 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ANDROID_V4L2_CODEC2_COMPONENTS_VIDEO_FRAME_POOL_H
+#define ANDROID_V4L2_CODEC2_COMPONENTS_VIDEO_FRAME_POOL_H
+
+#include <memory>
+#include <queue>
+
+#include <C2Buffer.h>
+#include <base/callback.h>
+#include <base/memory/weak_ptr.h>
+#include <base/sequenced_task_runner.h>
+
+#include <size.h>
+#include <v4l2_codec2/components/VideoFrame.h>
+#include <v4l2_codec2/components/VideoTypes.h>
+
+namespace android {
+
+// Fetch C2GraphicBlock from C2BlockPool and wrap to VideoFrame.
+// Provide asynchronous call which avoid the caller busy-polling while
+// C2BlockPool::fetchGraphicBlock() times out.
+class VideoFramePool {
+public:
+    using GetVideoFrameCB = base::OnceCallback<void(std::unique_ptr<VideoFrame>)>;
+
+    // |blockPool| is the C2BlockPool that we fetch graphic blocks from.
+    // |size| is the resolution size of the required graphic blocks.
+    // |pixelFormat| is the pixel format of the required graphic blocks.
+    // |isSecure| indicates the video stream is encrypted or not.
+    // All public methods and the callbacks should be run on |taskRunner|.
+    VideoFramePool(std::shared_ptr<C2BlockPool> blockPool, const media::Size& size,
+                   HalPixelFormat pixelFormat, bool isSecure,
+                   scoped_refptr<::base::SequencedTaskRunner> taskRunner);
+    ~VideoFramePool();
+
+    // Get a VideoFrame instance, which will be passed via |cb|.
+    // If any error occurs, then pass nullptr.
+    void getVideoFrame(GetVideoFrameCB cb);
+
+    // Return true if any callback of getting VideoFrame instance is pending.
+    bool hasPendingRequests() const;
+
+private:
+    void tryFetchGraphicBlock();
+    void sendVideoFrame(std::unique_ptr<VideoFrame> frame);
+
+    std::shared_ptr<C2BlockPool> mBlockPool;
+    const media::Size mSize;
+    const HalPixelFormat mPixelFormat;
+    const C2MemoryUsage mMemoryUsage;
+
+    std::queue<GetVideoFrameCB> mCbQueue;
+    size_t mNumRetries = 0;
+
+    scoped_refptr<::base::SequencedTaskRunner> mTaskRunner;
+    base::WeakPtr<VideoFramePool> mWeakThis;
+    base::WeakPtrFactory<VideoFramePool> mWeakThisFactory{this};
+};
+
+}  // namespace android
+
+#endif  // ANDROID_V4L2_CODEC2_COMPONENTS_VIDEO_FRAME_POOL_H
